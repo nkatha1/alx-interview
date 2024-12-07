2f8e0beff77a01f9532de60cd824c1e89528284b
nkatha1 <nkathapatience1@gmail.com>
2024-12-07 08:57:35 +0000

    files

---
+++ alx-files_manager/controllers/UsersController.js
@@ -0,0 +1,34 @@
+import dbClient from '../utils/db';
+import sha1 from 'sha1';
+
+export default class UsersController {
+  static async postNew (req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+    }
+
+    const user = await (await dbClient.usersCollection()).findOne({ email });
+
+    if (user) {
+      res.status(400).json({ error: 'user already exists' });
+      return;
+    }
+    const insertUser = await (await dbClient.usersCollection())
+      .insertOne({ email, password: sha1(password) });
+    const userId = insertUser.insertId.toString();
+
+    res.status(201).json({ email, id: userId });
+  }
+
+  static async getMe (req, res) {
+    const { user } = req;
+    res.status(200).json({ email: user.email, id: user._id.toString() });
+  }
+}
